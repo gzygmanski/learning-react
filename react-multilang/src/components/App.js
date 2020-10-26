57ee9eb0214c0592c8075d2cac4774d6555e3026
Grzegorz Zygmański <gzygmanski@hotmail.com>
2020-10-26 14:41:59 +0100

    training: context

---
+++ react-multilang/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import UserCreate from "./UserCreate";
+import LanguageSelector from "./LanguageSelector";
+import { LanguageStore } from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value={"primary"}>
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    );
+  }
+}
+
+export default App;
